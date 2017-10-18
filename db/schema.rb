4bf73f60a4c2ecb28ae79257c3a82905021e2526
delosiliana <dronixa@mail.ru>
2017-10-18 22:38:27 +0300

    create routes to trains association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018190940) do
+ActiveRecord::Schema.define(version: 20171018192718) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -40,7 +40,9 @@ ActiveRecord::Schema.define(version: 20171018190940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "current_station_id"
+    t.integer "route_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
   create_table "users", force: :cascade do |t|
