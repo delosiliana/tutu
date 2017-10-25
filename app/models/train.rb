4e4d8b4182ee5f6e9bd028a6d11af1226b3d3419
delosiliana <dronixa@mail.ru>
2017-10-25 23:16:52 +0300

    routes fix form

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,6 @@
 class Train < ApplicationRecord
-  belongs_to :current_station, class_name: 'RailwayStation', optional: true #пока для тестов потом уберу
-  belongs_to :route, optional: true #пока для тестов потом уберу
+  belongs_to :current_station, class_name: 'RailwayStation', optional: true
+  belongs_to :route, optional: true
   has_many :tickets
 
   validates :number, presence: true
