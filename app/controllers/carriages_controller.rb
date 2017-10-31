3adce5bedcb4627c7f65312e7257a79e39e4deef
delosiliana <dronixa@mail.ru>
2017-10-31 16:57:35 +0300

    add carriage controller

---
+++ app/controllers/carriages_controller.rb
@@ -0,0 +1,49 @@
+class CarriagesController < ApplicationController
+  before_action :set_carriage, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @carriages = Carriage.all
+  end
+
+  def show
+  end
+
+  def new
+    @carriage = Carriage.new
+  end
+
+  def create
+    @carriage = Carriage.new(carriage_params)
+    if @carriage.save
+      redirect_to @carriage, notice: 'Вагон создан'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @carriage.update(carriage_params)
+      redirect_to @carriage, notice: 'Вагон обновлен'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @carriage.destroy
+    redirect_to carriages_path, notice: 'Вагон удален'
+  end
+
+  private
+
+  def set_carriage
+    @carriage = Carriage.find(params[:id])
+  end
+
+  def carriage_params
+    params.require(:carriage).permit(:train_id, :carriage_type, :upper_seats, :lower_seats)
+  end
+end
