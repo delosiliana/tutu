168bcdf7d6cef094f6f2d773c12e26c6aeab0195
delosiliana <dronixa@mail.ru>
2017-11-13 19:37:46 +0600

    add railwaystation position routes

---
+++ db/migrate/20171113125549_add_station_position_railway_station_to_routes.rb
@@ -0,0 +1,5 @@
+class AddStationPositionRailwayStationToRoutes < ActiveRecord::Migration[5.1]
+  def change
+    add_column :railway_stations_routes, :station_position, :integer, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113123405) do
+ActiveRecord::Schema.define(version: 20171113125549) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "number"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20171113123405) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "station_position", default: 0
   end
 
   create_table "routes", force: :cascade do |t|
