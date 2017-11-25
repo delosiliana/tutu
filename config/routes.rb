f70eeb94dc56f3425da8b317aca157889f3ee19e
delosiliana <dronixa@mail.ru>
2017-11-25 16:09:53 +0300

    add search routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :routes
+
+  resource :search, only: [:new, :show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
