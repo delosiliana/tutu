ddabfc063247f678a8ef4c8178394144f6fcb9d9
delosiliana <dronixa@mail.ru>
2017-12-04 08:32:25 +0300

    edir search create on show controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   resources :tickets
 
-  resource :search, only: [:create, :show]
+  resource :search, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
