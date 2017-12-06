4ff88cfc76ad2d5a0197437ebf3f34797d9d7c4c
delosiliana <dronixa@mail.ru>
2017-12-06 07:23:26 +0300

    add redirect for admin or user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      welcome_index_path
+    else
+      search_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
