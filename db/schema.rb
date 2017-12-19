37df36ad6366086a1b0742644ddb104efd8fe2fe
delosiliana <dronixa@mail.ru>
2017-12-19 13:13:13 +0300

    add index carriages type and id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206041537) do
+ActiveRecord::Schema.define(version: 20171219095930) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer "number"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171206041537) do
     t.datetime "updated_at", null: false
     t.integer "sitting_seats"
     t.string "type"
+    t.index ["id", "type"], name: "index_carriages_on_id_and_type"
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
